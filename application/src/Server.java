d57acbd9bf4c3b644bfa0d71b221ec55b64a46d5
Nghviet <44728972+Nghviet@users.noreply.github.com>
2019-11-14 22:20:44 +0700

    Update

--- application/src/Server.java
+++ application/src/Server.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Server {
     private static Socket socket = null;
@@ -9,17 +12,16 @@ public class Server {
     private static DataInputStream in = null;
     private static DataOutputStream out = null;
 
-    private class Score implements Comparable<Score>{
+    private static class Score implements Comparable<Score>{
         private String name;
         private int score;
-
         Score(String name,int score) {
             this.name = name;
             this.score = score;
         }
 
         public String toString() {
-            return null;
+            return name+" "+score;
         }
 
         @Override
@@ -28,10 +30,23 @@ public class Server {
             if(this.score < that.score) return -1;
             return 0;
         }
+
     }
+
+    private static Score[] storage = new Score[10];
 
     public static void main(String[] args) throws IOException {
         server = new ServerSocket(port);
+
+        File storeF = new File("application/src/storage").getAbsoluteFile();
+        Scanner scanner = new Scanner(storeF);
+
+        for(int i=0;i<10;i++) {
+            String name = scanner.next();
+            int score = scanner.nextInt();
+            storage[i] = new Score(name,score);
+        }
+
         while(true)
         {
             socket = server.accept();
@@ -44,10 +59,15 @@ public class Server {
                 try {
                     line = in.readUTF();
                     if(line.equals("update")) {
-                        System.out.println("sending response");
-                        out.writeUTF("roger");
-                        out.writeUTF("over");
+                        System.out.println(line);
+                        String[] res = value();
+                        for(int i=0;i<10;i++) out.writeUTF(res[i]);
                         out.flush();
+                        break;
+                    }
+
+                    if(line.equals("add")) {
+
                     }
                     System.out.println(line);
                 } catch (IOException io) {
@@ -59,7 +79,18 @@ public class Server {
         }
     }
 
-    private String[] highValue() {
-        return null;
+    private static String[] value() {
+        String[] ret = new String[10];
+        for(int i=0;i<10;i++) ret[i] = storage[i].toString();
+        return ret;
     }
+
+    private void add(String name,int score) {
+        Score t = new Score(name,score);
+        if(storage[0].compareTo(t) < 0) {
+            storage[0] = t;
+            Arrays.sort(storage);
+        }
+    }
+
 }
